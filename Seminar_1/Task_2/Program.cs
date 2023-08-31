82394991165f825d8821fb090abb91cc5d5aac87
Alexandr Vinogradov <izum709@.com>
2023-08-31 21:05:12 +0300

    add var 2 Task_1

---
+++ Seminar_1/Task_2/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Напишите программу которая на вход принимает число и возвращает квадрат этого числа
+// число умноженное само на себя используя конвертацию и метод вывода в строку
+Console.WriteLine(" Hello Square =) ");
+Console.Write(" add value -> ");
+int a = Convert.ToInt32(Console.ReadLine());
+int sqr = a * a;
+// Console.Write("  input -> ");
+// Console.WriteLine(a);
+// Console.Write("  Square = ");
+// Console.WriteLine(sqr);
+Console.Write($"квадратом числа {a} евляется число {sqr}");
