c56cd1adcbac26da686931f27f61cd1a8566676c
Alexandr Vinogradov <izum709@.com>
2023-08-31 23:11:42 +0300

    add numbers -N to N

---
+++ Seminar_1/Task_5/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу которая на вход принимает число, а на выходе показывает все числа от -N до N
+Console.Clear();
+Console.WriteLine("Hello, Numbers =) ");
+Console.WriteLine("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+Console.WriteLine("Введите число -> ");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = -n;
+while (count <= n )
+{
+    Console.Write($"{count}, ");
+    count += 1;
+}
+
