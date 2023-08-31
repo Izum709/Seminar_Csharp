81eebb00f2188ab1923caca08a484ceb02415937
Alexandr Vinogradov <izum709@.com>
2023-08-31 23:24:39 +0300

    add Task_6 last number

---
+++ Seminar_1/Task_6/Program.cs
@@ -0,0 +1,11 @@
+﻿// Написать программу которая на вход принимает трехзначное число 
+// а на выходе показывает последнюю цифру этого числа
+
+
+Console.Clear();
+Console.WriteLine("Hello, third number");
+Console.WriteLine("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+Console.WriteLine("Введите 3х значное число -> ");
+int n = Convert.ToInt32(Console.ReadLine());
+int number = n % 10;
+Console.WriteLine(number);
