2fcb57f3205f261deddc134902fb3d705117b77b
Alexandr Vinogradov <izum709@.com>
2023-08-31 20:27:50 +0300

    add Task_1

---
+++ Seminar_1/Task_1/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу которая на вход принимает число и возвращает квадрат этого числа
+// число умноженное само на себя
+Console.WriteLine(" Hello Square =) ");
+int a = 4;
+Console.Write("  input -> ");
+Console.WriteLine(a);
+Console.Write("  Square = ");
+Console.WriteLine(a * a);
