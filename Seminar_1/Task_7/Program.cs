eb612e9db4f24cfac6b40a6d588c71da43ebb455
Alexandr Vinogradov <izum709@.com>
2023-09-01 17:50:20 +0300

    add task_7 max/min

---
+++ Seminar_1/Task_7/Program.cs
@@ -0,0 +1,21 @@
+﻿// Написать программу которая на вход принимает два числа и выдает какое число большее,
+//  а какое меньшее.
+//  a = 5, b = 7 -> max = 7
+
+
+Console.WriteLine("Hello Comparsion");
+Console.Clear();
+Console.WriteLine("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+Console.Write("Add number a =  ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Add number b =  ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+    Console.Write($"Max = {a}");
+}
+else
+{
+    Console.Write($"Max = {b}");
+
+}
