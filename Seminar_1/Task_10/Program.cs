52fed79d3e3dd55f8ccbfa1bc99f1e2d65dbe644
Alexandr Vinogradov <izum709@.com>
2023-09-02 08:59:26 +0300

    add task_10 Write(n % 2 == 0)

---
+++ Seminar_1/Task_10/Program.cs
@@ -0,0 +1,19 @@
+﻿// Написать программу которая которая на вход принимает число, а на выходе показывает
+// все четные числа от 1 до N
+
+
+Console.Clear();
+Console.WriteLine("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+Console.WriteLine("=) %2 =)");
+Console.Write("Add number N =  ");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+while (count < n)
+{
+    
+    if (count % 2 == 0)
+    {
+        Console.Write($"{count}, ");
+    }
+    count += 1;
+}   
