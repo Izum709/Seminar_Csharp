83fb39e32d3c5684b9862d47f5bfa618511520b6
Alexandr Vinogradov <izum709@.com>
2023-09-02 08:44:29 +0300

    add Task_9 четное/нечетное

---
+++ Seminar_1/Task_9/Program.cs
@@ -0,0 +1,9 @@
+﻿// Написать программу которая принимает на ввод число и выдает евляется ли оно четным
+
+Console.WriteLine("=) %2 =)");
+Console.Clear();
+Console.WriteLine("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+Console.Write("Add number a =  ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a % 2 == 0) Console.WriteLine($"{a} евляется четным");
+else Console.WriteLine($"{a} евляется НЕ четным");
