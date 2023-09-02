546a618189d69894bfa96bed505fd4ea48cb8253
Alexandr Vinogradov <izum709@.com>
2023-09-02 17:23:29 +0300

    Добавили поиск 2й цифры 3х значного числа

---
+++ Seminar_2/Task_10/Program.cs
@@ -0,0 +1,17 @@
+﻿// Написать программу которая принимает на вход трехзначное число, 
+// а на выходе показывает 2ю цифру этого числа
+
+
+int MiddleNumber(int arg)
+{
+    int middle = (arg / 10) % 10;
+    return middle;
+}
+
+
+Console.Clear();
+Console.WriteLine("Hello, Middle number!");
+Console.Write("add number -> ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Вторая цифра числа {number} это {MiddleNumber(number)}");
+
