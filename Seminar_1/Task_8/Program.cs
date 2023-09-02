ac663c9b84c4df2ffc5b2d29aa4e8652eb2630b6
Alexandr Vinogradov <izum709@.com>
2023-09-02 08:38:02 +0300

    add Task_8 min/max

---
+++ Seminar_1/Task_8/Program.cs
@@ -0,0 +1,15 @@
+﻿// Написать программу которая на вход принимает 3 числа и выдает максимальное из этих чисел
+
+Console.WriteLine("Hello, Min/max");
+Console.Clear();
+Console.WriteLine("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+Console.Write("Add number a =  ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Add number b =  ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Add number c =  ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (max < b) max = b;
+if (max < c) max = c;
+Console.WriteLine(max);
