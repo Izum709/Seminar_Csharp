9ca7db54732a08dd5096da79af7fc1d64ce7e022
Alexandr Vinogradov <izum709@.com>
2023-09-02 15:53:03 +0300

    add Task_1 Random function

--- Seminar_2/Task_1/Program.cs
+++ Seminar_2/Task_1/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу которая выводит случайное число из отрезка от 10 до 99 и 
+// показывает наибольшую цифру числа
+Console.Clear();
+Console.WriteLine("Hello, Random!");
+int number = new Random().Next(10, 99);
+Console.WriteLine(number);
+int a1 = number / 10;
+int a2 = number % 10;
+if (a1 > a2)
+{
+    Console.WriteLine($"В числе {number} цифра {a1} больше чем {a2}");
+}
+if( a1 == a2)
+{
+    Console.WriteLine($"В числе {number} цифра {a1} равна {a2}");
+}
+else
+{
+    Console.WriteLine($"В числе {number} цифра {a2} больше чем {a1}");
+}
