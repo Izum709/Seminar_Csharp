0e211dc98ed39284cb47f72a0fcf6f26c6f53a1d
Alexandr Vinogradov <izum709@.com>
2023-09-02 17:10:20 +0300

    Добавили прог кратности с использованием амперсанта -  &

---
+++ Seminar_2/Task_14/Program.cs
@@ -0,0 +1,22 @@
+﻿// Написать программу которая принимает на вход число и выводит 
+// является ли оно одновременно кратным 7 и 23
+
+void Multiplisity(int arg)
+{
+    if (arg % 7 == 0 && arg % 23 == 0)
+    {
+        Console.WriteLine("-> ДА");
+    }
+    else
+    {
+        Console.WriteLine("-> НЕТ");
+    }
+}
+
+
+Console.Clear();
+Console.WriteLine("Hello, multiplisity");
+Console.WriteLine("Введите число -> ");
+int number = Convert.ToInt32(Console.ReadLine());
+Multiplisity(number);
+
