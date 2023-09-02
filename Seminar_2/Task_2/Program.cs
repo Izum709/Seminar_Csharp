e998ebd7d045f3d7ee3c0844075b1549888f8073
Alexandr Vinogradov <izum709@.com>
2023-09-02 16:16:10 +0300

    Исключить среднюю цифру

---
+++ Seminar_2/Task_2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Написать программу которая выводит рандомное 3х значное число и удаляет 2ю цифру
+
+
+void Numbers(int number)
+{
+    int lust = number % 10;
+    int middle = (number /10) % 10;
+    int first = number / 100;
+    Console.Write(first);
+    Console.Write(lust);
+}
+
+
+Console.Clear();
+Console.WriteLine("Hello, Random!");
+int number = new Random().Next(100, 999);
+Console.WriteLine(number);
+Numbers(number);
