67681282eb56e165bb316597e30c2adec96f7952
Alexandr Vinogradov <izum709@.com>
2023-09-03 18:40:33 +0300

    Add Task_18

---
+++ Seminar_3/Task_18/Program.cs
@@ -0,0 +1,19 @@
+﻿//  Написать программу которая по введенной четверти выдает диапазон
+// возможных значений X & Y
+
+
+
+Console.Clear();
+int EnterData(string data)
+{
+    Console.Write(data);
+    int intenger = Convert.ToInt32(Console.ReadLine());
+    return (intenger);
+}
+
+
+int data = EnterData("Введите номер четверти ");
+if (data == 1) Console.WriteLine(" x > 0 & y > 0");
+if (data == 2) Console.WriteLine(" x < 0 & y > 0");
+if (data == 3) Console.WriteLine(" x < 0 & y < 0");
+if (data == 4) Console.WriteLine(" x > 0 & y < 0");
