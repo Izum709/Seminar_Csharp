21a29a0714784cbf889498944b2a1c76d2380fad
Alexandr Vinogradov <izum709@.com>
2023-09-03 22:18:34 +0300

    add Task_22

---
+++ Seminar_3/Task_22/Program.cs
@@ -0,0 +1,27 @@
+﻿// Написать программу которая на вход принимает число и 
+// выдает таблицу квадратных чисел от 1 до N
+
+Console.Clear();
+Console.WriteLine("Hello, Sqr!");
+int EnterData(string data)
+{
+    Console.Write(data);
+    int intenger = Convert.ToInt32(Console.ReadLine());
+    return (intenger);
+}
+
+
+void SQR (int arg)
+{
+    int count = 1;
+    while (count <= arg)
+    {
+        int answer = count * count;
+        Console.Write($"{answer}, ");
+        count = count + 1;
+    }
+}
+
+
+int n = EnterData("add N -> ");
+SQR(n);
