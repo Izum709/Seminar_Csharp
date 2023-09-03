abe6d8e787c0f69273cb72782995e6366ffe540e
Alexandr Vinogradov <izum709@.com>
2023-09-03 18:30:43 +0300

    add Function EnterData

---
+++ Seminar_3/Task_17.2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Написать программу которая на вход принимает координаты x и y а на выходе указывает 
+// четверть расположения точки используя функции EnterData
+
+Console.Clear();
+Console.WriteLine("Hello, Function!");
+
+int EnterData(string data)
+{
+    Console.WriteLine(data);
+    int intenger = Convert.ToInt32(Console.ReadLine());
+    return (intenger);
+}
+
+
+int x = EnterData("Введите данные -> ");
+int y = EnterData("Введите данные -> ");
+if(x > 0 && y > 0) Console.WriteLine(" 1 четверть ");
+if(x > 0 && y < 0) Console.WriteLine(" 4 четверть ");
+if(x < 0 && y < 0) Console.WriteLine(" 3 четверть ");
+if(x < 0 && y > 0) Console.WriteLine(" 2 четверть ");
