3f232243746a93174a40a02535522b2ac13aa9be
Alexandr Vinogradov <izum709@.com>
2023-09-07 18:25:41 +0300

    add Task-17

---
+++ Seminar_3/Task_19/Program.cs
@@ -0,0 +1,35 @@
+﻿// Написать прогрпмму которая на вход принимает пятизначное число
+//  и проверяет евляется ли оно палиндромом.
+
+Console.Clear();
+Console.WriteLine("Hello, Палиндром");
+
+
+void Polindrom(int arg)
+{
+    int a = arg / 10000;
+    int b = (arg / 1000) % 10;
+    int c = (arg % 100) / 10;
+    int d = arg % 10;
+    Console.Write($"{a}, {b}, {c}, {d} -> ");
+    if (a == d && c == b)
+    {
+        Console.Write(" N -> Палиндром ");
+    }
+    else
+    {
+        Console.Write(" N -> Не палиндром ");
+    }
+}
+
+
+int EnterData(string data)
+{
+    Console.Write(data);
+    int intenger = Convert.ToInt32(Console.ReadLine());
+    return intenger;
+}
+
+int n = EnterData("Add n = -> ");
+Console.WriteLine(n);
+Polindrom(n);
