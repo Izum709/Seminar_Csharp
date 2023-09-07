7704adc0095078995afc4ff6f7e44c3e80f0ad49
Alexandr Vinogradov <izum709@.com>
2023-09-07 19:27:02 +0300

    Add task_28

---
+++ Seminar_4/Task_28/Program.cs
@@ -0,0 +1,28 @@
+﻿// Написать программу которая на ввод принимает число n и на выводе дает
+// произведение этих чисел от 1 до n
+
+Console.Clear();
+Console.WriteLine("Hello, * ");
+
+
+int EnterData(string data)
+{
+    Console.Write(data);
+    int intenger = Convert.ToInt32(Console.ReadLine());
+    return intenger;
+}
+
+void Composition (int arg)
+{
+    int count = 1;
+    int answer = 1;
+    while (count <= arg)
+    {
+        answer *= count;
+        count++; 
+    }
+    Console.WriteLine($"Composition = {answer}");
+}
+
+int n = EnterData("Enter N -> ");
+Composition(n);
