3e5ff337f92f01865a2f674262063a4cd7360d28
Alexandr Vinogradov <izum709@.com>
2023-09-07 18:50:09 +0300

    Task_23

---
+++ Seminar_3/Task_23/Program.cs
@@ -0,0 +1,24 @@
+﻿// Написать программу которая принимает на ввод число и выдает таблицу кубов от 1 до N
+Console.Clear();
+Console.WriteLine("Hello, Sqr!");
+int EnterData(string data)
+{
+    Console.Write(data);
+    int intenger = Convert.ToInt32(Console.ReadLine());
+    return (intenger);
+}
+
+void Qube (int n)
+{
+    int coint = 1;
+    while (coint <= n)
+    {
+        int answer = coint * coint * coint;
+        Console.Write($"{answer}  ");
+        coint++;
+    }
+}
+
+
+int n = EnterData("add N -> ");
+Qube (n);
