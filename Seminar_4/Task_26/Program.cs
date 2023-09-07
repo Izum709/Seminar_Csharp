6a777b2b7a1922b9bf4a2d417a676d06c1ecfe4d
Alexandr Vinogradov <izum709@.com>
2023-09-07 19:14:06 +0300

    add Task_26

---
+++ Seminar_4/Task_26/Program.cs
@@ -0,0 +1,27 @@
+﻿// Написать программу которая на ввод принимает число и выдает количество цифр в числе
+//  1234 -> 4
+Console.Clear();
+Console.WriteLine("Hello, Count!");
+
+
+int Count (int n)
+{
+    int count = 0;
+    while (n > 0)
+    {
+        n /= 10;
+        count++; 
+    }
+    return count;
+}
+
+
+int EnterData(string data)
+{
+    Console.Write(data);
+    int intenger = Convert.ToInt32(Console.ReadLine());
+    return intenger;
+}
+
+int n = EnterData("Enter N -> ");
+Console.Write(Count(n));
