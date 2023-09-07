fa644c3d4a74b21d0d4649e31c2946fd591a6259
Alexandr Vinogradov <izum709@.com>
2023-09-07 19:04:39 +0300

    add Task_24

---
+++ Seminar_4/Task_24/Program.cs
@@ -0,0 +1,27 @@
+﻿// Написать программу которая на вход принимает число и выдает сумму этих чисел
+// от 1 до N
+
+Console.Clear();
+Console.WriteLine("Hello, Summ");
+
+int EnterData(string data)
+{
+    Console.Write(data);
+    int intenger = Convert.ToInt32(Console.ReadLine());
+    return intenger;
+}
+
+int Summa(int n)
+{
+    
+    int summa = 0;
+    for (int i = 1; i <= n; i++)
+    {
+       summa = summa + i; 
+    }
+    return summa; 
+}
+
+int n = EnterData("Enter N -> ");
+Console.WriteLine(Summa(n));
+
